e207f322f92f5a9907d1beafe1ae6ab381614458
Kiril Panev <kiril.panev@project-a.com>
2020-05-27 15:20:07 +0200

    remove enums from geo_location add digitised zip_code to geo_location

--- app/data_integration/pipelines/e_commerce/transform_geo_location.sql
+++ app/data_integration/pipelines/e_commerce/transform_geo_location.sql
@@ -1,34 +1,30 @@
-SELECT util.create_enum(
-           'ec_dim_next.ZIP_CODE_PREFIX',
-           (SELECT array_agg(DISTINCT zip_code_prefix) FROM ec_tmp.geo_location));
-
-SELECT util.create_enum(
-           'ec_dim_next.CITY',
-           (SELECT array_agg(DISTINCT city) FROM ec_tmp.geo_location));
-
-SELECT util.create_enum(
-           'ec_dim_next.STATE',
-           (SELECT array_agg(DISTINCT state) FROM ec_tmp.geo_location));
-
 DROP TABLE IF EXISTS ec_dim_next.geo_location CASCADE;
 
 CREATE TABLE ec_dim_next.geo_location
 (
-  geo_location_id INTEGER                     NOT NULL PRIMARY KEY, -- Unique integer representation of the zip_code_prefix
+    geo_location_id  INTEGER NOT NULL PRIMARY KEY, -- Unique integer representation of the zip_code_prefix
 
-  zip_code_prefix ec_dim_next.ZIP_CODE_PREFIX NOT NULL,             -- First 5 digits of zip code
-  latitude        DOUBLE PRECISION,
-  longitude       DOUBLE PRECISION,
-  city            ec_dim_next.CITY            NOT NULL,
-  state           ec_dim_next.STATE           NOT NULL
+    zip_code_prefix  TEXT    NOT NULL,             -- First 5 digits of zip code
+    zip_code_digit_1 TEXT    NOT NULL,
+    zip_code_digit_2 TEXT    NOT NULL,
+    zip_code_digit_3 TEXT    NOT NULL,
+    zip_code_digit_4 TEXT    NOT NULL,
+    latitude         DOUBLE PRECISION,
+    longitude        DOUBLE PRECISION,
+    city             TEXT    NOT NULL,
+    state            TEXT    NOT NULL
 );
 
 INSERT INTO ec_dim_next.geo_location
 SELECT geo_location_id,
-       zip_code_prefix::ec_dim_next.ZIP_CODE_PREFIX AS zip_code_prefix,
-       latitude                                     AS latitude,
-       longitude                                    AS longitude,
-       city::ec_dim_next.CITY                       AS city,
-       state::ec_dim_next.STATE                     AS state
+       zip_code_prefix  AS zip_code_prefix,
+       zip_code_digit_1 AS zip_code_digit_1,
+       zip_code_digit_2 AS zip_code_digit_2,
+       zip_code_digit_3 AS zip_code_digit_3,
+       zip_code_digit_4 AS zip_code_digit_4,
+       latitude         AS latitude,
+       longitude        AS longitude,
+       city             AS city,
+       state            AS state
 FROM ec_tmp.geo_location;
 
--- app/schema/entities/geo_location.py
+++ app/schema/entities/geo_location.py
@@ -15,9 +15,24 @@ geo_location_entity.add_attribute(
     high_cardinality=True)
 geo_location_entity.add_attribute(
     name='Zip code',
-    description='First 5 digits of zip code',
-    column_name='zip_code_prefix',
-    type=Type.ENUM)
+    description='First 5 digits of the zip code',
+    column_name='zip_code_prefix')
+geo_location_entity.add_attribute(
+    name='Zip code 1st digit',
+    description='First digit of the zip code',
+    column_name='zip_code_digit_1')
+geo_location_entity.add_attribute(
+    name='Zip code 2nd digit',
+    description='First 2 digits of the zip code',
+    column_name='zip_code_digit_2')
+geo_location_entity.add_attribute(
+    name='Zip code 3rd digit',
+    description='First 3 digits of the zip code',
+    column_name='zip_code_digit_3')
+geo_location_entity.add_attribute(
+    name='Zip code 4th digit',
+    description='First 4 digits of the zip code',
+    column_name='zip_code_digit_4')
 geo_location_entity.add_attribute(
     name='Latitude',
     description='Latitude coordinate',
@@ -31,10 +46,8 @@ geo_location_entity.add_attribute(
 geo_location_entity.add_attribute(
     name='City',
     description='City name',
-    column_name='city',
-    type=Type.ENUM)
+    column_name='city')
 geo_location_entity.add_attribute(
     name='State',
     description='State name',
-    column_name='state',
-    type=Type.ENUM)
+    column_name='state')
