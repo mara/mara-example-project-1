e7d2e09d554e6ee4c518a2ded9333326c3642843
Kiril Panev <56028534+kirilpanev@users.noreply.github.com>
2020-05-27 15:24:15 +0200

    Refactor geo location (#4)
    * remove enums from geo_location
    * add digitised zip_code to geo_location

--- app/data_integration/pipelines/e_commerce/preprocess_geo_location.sql
+++ app/data_integration/pipelines/e_commerce/preprocess_geo_location.sql
@@ -2,47 +2,55 @@ DROP TABLE IF EXISTS ec_tmp.geo_location CASCADE;
 
 CREATE TABLE ec_tmp.geo_location
 (
-  geo_location_id  INTEGER NOT NULL, -- Unique integer representation of the zip_code_prefix
+    geo_location_id  INTEGER NOT NULL, -- Unique integer representation of the zip_code_prefix
 
-  zip_code_prefix TEXT    NOT NULL, -- First 5 digits of zip code
-  latitude        DOUBLE PRECISION,
-  longitude       DOUBLE PRECISION,
-  city            TEXT    NOT NULL,
-  state           TEXT    NOT NULL
+    zip_code_prefix  TEXT    NOT NULL, -- First 5 digits of zip code
+    zip_code_digit_1 TEXT    NOT NULL,
+    zip_code_digit_2 TEXT    NOT NULL,
+    zip_code_digit_3 TEXT    NOT NULL,
+    zip_code_digit_4 TEXT    NOT NULL,
+    latitude         DOUBLE PRECISION,
+    longitude        DOUBLE PRECISION,
+    city             TEXT    NOT NULL,
+    state            TEXT    NOT NULL
 );
 
 WITH geo_locations AS (
-  SELECT zip_code_prefix,
-         latitude      AS latitude,
-         longitude     AS longitude,
-         initcap(city) AS city,
-         state         AS state
-  FROM ec_data.geolocation
-  UNION ALL
-  SELECT zip_code_prefix,
-         NULL AS latitude,
-         NULL AS longitude,
-         city,
-         state
-  FROM ec_tmp.seller
-  UNION ALL
-  SELECT zip_code_prefix,
-         NULL AS latitude,
-         NULL AS longitude,
-         city,
-         state
-  FROM ec_tmp.customer
+    SELECT zip_code_prefix,
+           latitude      AS latitude,
+           longitude     AS longitude,
+           initcap(city) AS city,
+           state         AS state
+    FROM ec_data.geolocation
+    UNION ALL
+    SELECT zip_code_prefix,
+           NULL AS latitude,
+           NULL AS longitude,
+           city,
+           state
+    FROM ec_tmp.seller
+    UNION ALL
+    SELECT zip_code_prefix,
+           NULL AS latitude,
+           NULL AS longitude,
+           city,
+           state
+    FROM ec_tmp.customer
 )
 
 INSERT
 INTO ec_tmp.geo_location
-SELECT zip_code_prefix::INTEGER AS geo_location_id,
+SELECT zip_code_prefix::INTEGER      AS geo_location_id,
 
-       zip_code_prefix          AS zip_code_prefix,
-       min(latitude)            AS latitude,
-       min(longitude)           AS longitude,
-       min(city)                AS city,
-       min(state)               AS state
+       zip_code_prefix               AS zip_code_prefix,
+       substr(zip_code_prefix, 1, 1) AS zip_code_digit_1,
+       substr(zip_code_prefix, 1, 2) AS zip_code_digit_2,
+       substr(zip_code_prefix, 1, 3) AS zip_code_digit_3,
+       substr(zip_code_prefix, 1, 4) AS zip_code_digit_4,
+       min(latitude)                 AS latitude,
+       min(longitude)                AS longitude,
+       min(city)                     AS city,
+       min(state)                    AS state
 FROM geo_locations
 GROUP BY zip_code_prefix;
 
