cf8fdda26f2049e701cf2298325705b6f6e2b825
Martin Loetzsch <martin.loetzsch@gmail.com>
2020-06-12 10:06:53 +0200

    minor

--- app/config.py
+++ app/config.py
@@ -9,5 +9,5 @@ def data_dir():
 
 
 def first_date():
-    """The first date for which to download and integrate BigQuery data"""
+    """The first date for which to process data (can be used to limit data volumes on local machine)"""
     return datetime.date(2017, 1, 1)
