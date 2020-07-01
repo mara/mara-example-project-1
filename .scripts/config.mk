f7b32df7234cf7be1821fa855e479c1e087c76c5
Martin Loetzsch <martin.loetzsch@gmail.com>
2020-07-01 12:25:50 +0200

    minor

--- .scripts/config.mk
+++ .scripts/config.mk
@@ -4,8 +4,8 @@
 ensure-config: app/local_setup.py
 
 app/local_setup.py:
-	>&2 echo '!!! copied app/local_setup.py.example to app/local_setup.py. Please check it\n'
+	>&2 echo '!!! copied app/local_setup.py.example to app/local_setup.py. Please check\n'
 	cp -v app/local_setup.py.example app/local_setup.py
 
 .cleanup-config:
-	rm app/local_setup.py
+	rm -f app/local_setup.py
