cd3c766595a8976ee0dbcc5e8d8f5ec7b24e8bb9
Martin Loetzsch <martin.loetzsch@gmail.com>
2020-11-17 22:19:25 +0100

    Update mara-metabase to 2.0.0

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -1,26 +1,24 @@
 """Set up Navigation, ACL & Logos"""
 
-import mara_pipelines
-import mara_data_explorer
 import flask
 import mara_acl
 import mara_acl.users
 import mara_app
 import mara_app.layout
+import mara_data_explorer
 import mara_db
+import mara_metabase
+import mara_metabase.acl
+import mara_metabase.config
+import mara_mondrian
+import mara_mondrian.config
 import mara_page.acl
+import mara_pipelines
+import mara_schema
+from app.ui import start_page
 from mara_app import monkey_patch
 from mara_page import acl
 from mara_page import navigation
-import mara_mondrian.config
-import mara_mondrian
-import mara_metabase.config
-import mara_metabase
-
-import mara_schema
-import olist_ecommerce
-
-from app.ui import start_page
 
 blueprint = flask.Blueprint('ui', __name__, url_prefix='/ui', static_folder='static')
 
@@ -53,7 +51,7 @@ def acl_resources():
                             children=[*mara_data_explorer.MARA_ACL_RESOURCES().values(),
                                       *mara_metabase.MARA_ACL_RESOURCES().values(),
                                       *mara_mondrian.MARA_ACL_RESOURCES().values()
-                            ]),
+                                      ]),
             acl.AclResource(name='Admin',
                             children=[mara_app.MARA_ACL_RESOURCES().get('Configuration'),
                                       mara_acl.MARA_ACL_RESOURCES().get('Acl')])]
@@ -65,6 +63,9 @@ monkey_patch.patch(mara_page.acl.current_user_has_permissions)(mara_acl.permissi
 monkey_patch.patch(mara_page.acl.user_has_permissions)(mara_acl.permissions.user_has_permissions)
 
 monkey_patch.patch(mara_acl.config.whitelisted_uris)(lambda: ['/mara-app/navigation-bar', '/mondrian/saiku/authorize'])
+
+# enable user and permission sync from mara acl to Metabase
+mara_metabase.acl.enable_automatic_sync_of_users_and_permissions_to_metabase()
 
 
 # navigation bar (other navigation entries will be automatically added)
